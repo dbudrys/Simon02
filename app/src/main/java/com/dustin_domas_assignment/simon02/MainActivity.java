0dab5fdee8aef2a880e3e62a1e152ea2fbd7246c
Domo <dbudrys@my.apsu.edu>
2017-02-20 20:06:47 -0600

    Initial commit

---
+++ app/src/main/java/com/dustin_domas_assignment/simon02/MainActivity.java
@@ -0,0 +1,40 @@
+package com.dustin_domas_assignment.simon02;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button play_b = (Button) findViewById(R.id.play_button);
+        play_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),
+                        PlayActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        Button about_b = (Button) findViewById(R.id.about_button);
+        about_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        AboutActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+    }
+}
